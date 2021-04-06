2051263cb82651345c7e4101e1e60437ce6541b3
exc4l <cps0537@gmail.com>
2021-04-06 09:56:08 +0200

    basic docs

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-# kanjigrid
-Create Kanji Grids out of Text
-
-```python
-pip install kanjigrid
-```
-
-##  MWE
-```python
 import kanjigrid
 
 gridder = kanjigrid.Gridder("Kanji", 40, "Header", 52)
@@ -18,6 +9,3 @@ with open("test.txt", "r", encoding="utf-8") as f:
 gridder.feed_text(data)
 grid = gridder.make_grid(grading, outside_of_grading=True, stats=True, bar_graph=True)
 grid.save("test.png")
-```
-
-![](https://github.com/exc4l/kanjigrid/blob/main/test.png)
